d8e91bfa77defa16927959329c95b56dd803e017
abhishek <abhishekmazumder69@gmail.com>
2020-11-03 11:10:25 +0530

    axios cancel token implemented

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -12,7 +12,6 @@ const ACTIONS = {
 const BASE_URL =
 	'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json';
 
-
 function reducer(state, action) {
 	switch (action.type) {
 		case ACTIONS.MAKE_REQUEST:
@@ -31,22 +30,28 @@ function reducer(state, action) {
 	}
 }
 
-
 export default function useFetchJobs(params, page) {
 	const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
 
 	useEffect(() => {
+		const cancelToken = axios.CancelToken.source();
 		dispatch({ type: ACTIONS.MAKE_REQUEST });
 		axios
 			.get(BASE_URL, {
+				cancelToken: cancelToken.token,
 				params: { markdown: true, page: page, ...params },
 			})
 			.then(res => {
 				dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
 			})
 			.catch(err => {
+				if (axios.isCancel(err)) return;
 				dispatch({ type: ACTIONS.ERROR, payload: { error: err } });
 			});
+
+		return () => {
+			cancelToken.cancel();
+		};
 	}, [params, page]);
 
 	return state;
